b9e640fb1f896bb3f60d32b9a2ea0e92e18c6a52
Ryan Blankemeier <rblankemeier@predictivetechnologies.com>
2019-07-25 15:57:35 -0400

    test

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(request.text)
+    response.send(request.text);
 })
+
+console.log(request.text);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
