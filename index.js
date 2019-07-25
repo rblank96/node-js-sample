85e33d97d026c00a6b48d9d2d14f1759dd3a16b8
Ryan Blankemeier <rblankemeier@predictivetechnologies.com>
2019-07-25 18:11:10 -0400

    fsafsd

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ request(options, function (error, response, body) {
    );
    console.log(body);
 });
-}
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
