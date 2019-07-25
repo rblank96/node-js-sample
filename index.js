3dc906f63ebe3f48c6d849b91627cd3ab0573bc1
Alex <lichena@wharton.upenn.edu>
2019-07-25 17:42:15 -0400

    added test route for search

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const request = require('request')
 const express = require('express');
 const os = require('os');
 const fs = require('fs');
@@ -14,16 +15,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-    
-    response.send("hello");
+app.get('/', function(req, res) {
+    res.send("hello");
 })
 
-app.post('/', function(request, response) {
-    console.log(request.body.text);
-    response.send("you said: " + request.body.text);
-})
-
+app.post('/search', function(req, res) {
+  request
+    .get('https://slackoverflow.atlassian.net/wiki/rest/api/search?cql=type="ac:com.atlassian.confluence.plugins.confluence-questions:question"',
+    {
+      'auth': {
+        'user': 'alichen@predictivetechnologies.com',
+        'pass': 'QJf4In3V9BhK7rEgdehrC726',
+        'sendImmediately': false
+      }
+    })
+    .on('response', function(response) {
+      res.send(respond)
+    })
+    .on('error', function(err) {
+      res.send("error"")
+    })
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
