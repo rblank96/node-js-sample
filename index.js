80419b76f536a42714c472f950324b22145046aa
Ryan Blankemeier <rblankemeier@predictivetechnologies.com>
2019-07-25 16:09:24 -0400

    test

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-    response.send(request.body['text']);
+    obj = JSON.parse(request.body);
+    response.send(obj.text);
 })
 
 
