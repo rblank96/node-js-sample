dd4c7b4da8ad38d161ae010b8e0d9c9de5a47918
Ryan Blankemeier <rblankemeier@predictivetechnologies.com>
2019-07-25 18:09:39 -0400

    prayers

--- index.js
+++ index.js
@@ -19,14 +19,33 @@ app.get('/', function(req, res) {
     res.send("hello");
 })
 
+
+
 app.post('/search', function(req, res) {
+    var options = {
+   method: 'GET',
+   url: 'https://slackoverflow.atlassian.net/wiki/rest/api/search?cql=type="ac:com.atlassian.confluence.plugins.confluence-questions:question"',
+   auth: { username: 'alichen@predictivetechnologies.com', password: 'QJf4In3V9BhK7rEgdehrC726' },
+   headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+   }
+   };
+  /*
   request
     .get('https://slackoverflow.atlassian.net/wiki/rest/api/search?cql=type="ac:com.atlassian.confluence.plugins.confluence-questions:question"',
     {
-      'auth': {
-        'user': 'alichen@predictivetechnologies.com',
-        'pass': 'QJf4In3V9BhK7rEgdehrC726'
-      }
+	'auth': {
+        'username': 'alichen@predictivetechnologies.com',
+        'password': 'QJf4In3V9BhK7rEgdehrC726'
+	},
+
+	headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+	}
+
+	
     })
     .on('response', function(response) {
       console.log(response)
@@ -36,6 +55,15 @@ app.post('/search', function(req, res) {
       res.send("error")
     })
 });
+  */
+request(options, function (error, response, body) {
+   if (error) throw new Error(error);
+   console.log(
+      'Response: ' + response.statusCode + ' ' + response.statusMessage
+   );
+   console.log(body);
+});
+}
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
